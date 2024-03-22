b95de12302ca3f8231389c495de4d627b1584e36
henrymontesanoiiiccm <henrymontesanoiii@gmail.com>
2024-03-22 11:38:44 -0400

    preliminary javascript to pull product & related comments from database & console log data

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # ReviewRater
-Repository for Montclair State University: Software Engineering II Project - Sentiment Analysis for Product Rating
+[![License](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+
+Welcome to the Repository for Montclair State University: Software Engineering II Project - Sentiment Analysis for Product Rating! This application will allow users to login tot he application to view a list of products and individual product details. Users will be able to submit comments which the application will interpret into a star rating on a scale of 1-5 stars. The application will also display the product's overall 5-star rating based on the individual 5-star rating for each submitted comment.
+
+## Features
+
+- **Products Display**: Users will be able to view multiple products, inlucding their images and descriptions that have been added to the application.
+- **Product Details**: Users will be able to view individual product details, including the product's 5-star rating and submitted user comments.
+- **User Profiles**: Users can register a new profile in order to submit a product comment.
+- **Responsive Design**: Access the application seamlessly across devices - desktop, tablet, or mobile from any supported web browser.
+
---
+++ javas/details.js
@@ -0,0 +1,79 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs, doc, getDoc } from 'firebase/firestore/lite';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBou6XmpPSnhcoGk4rsvap_nNVtChBh7xg",
+  authDomain: "reviewrater-b4cfd.firebaseapp.com",
+  projectId: "reviewrater-b4cfd",
+  storageBucket: "reviewrater-b4cfd.appspot.com",
+  messagingSenderId: "1024129262624",
+  appId: "1:1024129262624:web:7b2460bc9f15014c32c845"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+//Initialize Product Variables
+
+// Get products list
+async function getProduct(db) {
+  /*
+  //gets all products
+  let prodName = "";
+  let prodDesc = "";
+  const productCol = collection(db, 'products');
+  const productSnapshot = await getDocs(productCol);
+  const productsList = productSnapshot.docs.map(doc => doc.data());
+  console.log(productsList)
+  */
+
+  let prodName = "";
+  let prodDesc = "";
+  let prodRating = 0;
+  const productID = "VH9oGG3pusaizEanXDTS";
+
+  const docRef = doc(db, 'products', productID);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    prodName = docSnap.data().name;
+    prodDesc= docSnap.data().description;
+    prodRating= docSnap.data().rating;
+    //IMPORTANT: RESTORE WHEN USING BROWSER 
+    /*document.getElementById("title").innerHTML = prodName;
+    document.getElementById("desc").innerHTML = prodDesc;
+    */
+    console.log("Product: \n")
+    console.log(prodName);
+    console.log(prodRating);
+    console.log(prodDesc);
+    console.log("\n")
+    
+  } else {
+  // docSnap.data() will be undefined in this case
+    console.log("No such document!");
+  } 
+
+  //comments
+  const commentCol = collection(db, 'comments');
+  const commentSnapshot = await getDocs(commentCol);
+  const commentList = commentSnapshot.docs.map(doc => doc.data());
+  let comments = [];
+  //return specified product
+  for (let i = 0; i < commentList.length; i++) { 
+    if (commentList[i].product==productID){
+      comments.push(commentList[i]);
+    }
+   
+  }
+  console.log("Comments: \n")
+  for (let j = 0; j < comments.length; j++) {
+    console.log(comments[j]);
+  }
+
+}
+getProduct(db);
