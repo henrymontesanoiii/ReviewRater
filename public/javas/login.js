7e97bd2e40939e920d5bceb87ccef24aeb199dad
Andrew Sirico <siricoa1@montclair.edu>
2024-03-23 22:36:23 -0400

    firebase create/read routes created
    have create/read routes working for the reviewer collection. requires key.json which is ignored on the push for security reasons.

--- public/javas/login.js
+++ public/javas/login.js
@@ -22,3 +22,28 @@ backBtn.onclick = function myFunction() {
     signUpContainer.style.display = "block";
   };
 };
+
+document.getElementById('createUserForm').addEventListener('submit', async function(event) {
+  event.preventDefault();
+  
+  const formData = new FormData(this);
+  const data = {};
+  formData.forEach((value, key) => {
+      data[key] = value;
+  });
+
+  console.log(data);
+  const response = await fetch('/create', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+  });
+
+  if (response.ok) {
+      alert('User created successfully');
+  } else {
+      alert('Error creating user');
+  }
+});
