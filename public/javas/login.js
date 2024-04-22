4b5174217f7a00c4a4688e84e310e77ed0212289
Andrew Sirico <siricoa1@montclair.edu>
2024-04-22 01:05:54 -0400

    Logout buttons and some extra changes
    Got some stuff up to date. Login/logout functionality and user password and name requirements.

--- public/javas/login.js
+++ public/javas/login.js
@@ -32,14 +32,12 @@ document.getElementById('createUserForm').addEventListener('submit', async funct
   
   const formData = new FormData(this);
   const data = {};
-  if(formData.get("firstname") == null || nfInput.value == "" || /[^a-zA-Z]/.test(nfInput.value)){
+  if(formData.get("firstname") == null || formData.get("firstname") == "" || /[^a-zA-Z]/.test(formData.get("firstname"))){
     alert("Please enter your first name");
-  } else if(formData.get("lastname") == null || nlInput.value == "" || /[^a-zA-Z]/.test(nlInput.value)) {
+  } else if(formData.get("lastname") == null || formData.get("lastname") == "" || /[^a-zA-Z]/.test(formData.get("lastname"))) {
       alert("Please enter your last name");
-  } else if(formData.get("email") == null || formData.get("email") == "" || /[^a-zA-Z]/.test(formData.get("email"))){
-
-  } else if(formData.get("email") == null || formData.get("email") == "" || /[^a-zA-Z]/.test(formData.get("email"))){
-
+  } else if(formData.get("password") == null || formData.get("password") == "" || /[^a-zA-Z0-9]/.test(formData.get("password")) || formData.get("password").length < 8){
+    alert("password must be at least 8 characters long!");
   }  else {
     formData.forEach((value, key) => {
       data[key] = value;
